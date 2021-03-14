443e538c2e4da148fded95ee11ddd2b08944951c
XRP97 <Github97>
2021-03-13 22:13:03 -0500

    Feature: Setup calculator with result property

--- src/Calculator.py
+++ src/Calculator.py
@@ -1,23 +1,9 @@
-def addition(a, b):
-    c = a + b
-    return c
-
-
-def subtraction(a, b):
-    c = a - b
-    return c
-
 
 class Calculator:
     result = 0
 
     def __init__(self):
+        x = 2 + 2
+        self.result = x;
         pass
 
-    def __add__(self, a, b):
-        self.result = addition(a, b)
-        return self.result
-
-    def subtract(self, a, b):
-        self.result = subtraction(a, b)
-        return self.result
